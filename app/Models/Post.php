d0bbdcd0cd0120905960987961029814b84204e4
arifhossen-dev <ahak.bsl@gmail.com>
2024-03-12 10:45:15 +0600

    Post BelongsTo an Author relation created

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -15,4 +16,10 @@ class Post extends Model
         'slug',
         'body',
     ];
+
+
+    function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
