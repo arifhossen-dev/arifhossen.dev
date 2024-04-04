da0e39c26f1155f7ca8300d3c61c30dd8e8d58c6
arifhossen-dev <ahak.bsl@gmail.com>
2024-04-04 23:31:52 +0600

    TextArea form component created

---
+++ resources/js/Components/TextArea.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const model = defineModel({
+    type: String,
+    required: false,
+});
+
+const textarea = ref(null);
+
+onMounted(() => {
+    if (textarea.value.hasAttribute('autofocus')) {
+        textarea.value.focus();
+    }
+});
+
+defineExpose({ focus: () => textarea.value.focus() });
+</script>
+
+<template>
+    <textarea
+        class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        v-model="model"
+        ref="textarea"
+    ></textarea>
+</template>
