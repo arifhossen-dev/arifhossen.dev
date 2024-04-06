b339044dbacb44b6f8be85b55ff33dea521dc2be
arifhossen-dev <ahak.bsl@gmail.com>
2024-04-06 11:48:46 +0600

    Markdown feature added to tiptap editor with tailwind css styling

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -3,6 +3,7 @@
 import {EditorContent, useEditor} from "@tiptap/vue-3";
 import {StarterKit} from "@tiptap/starter-kit";
 import {watch} from "vue";
+import {Markdown} from "tiptap-markdown";
 
 const props = defineProps({
     modelValue:'',
@@ -13,12 +14,18 @@ const emit = defineEmits(['update:modelValue']);
 const editor = useEditor({
     extensions:[
         StarterKit,
+        Markdown,
     ],
-    onUpdate:()=>emit('update:modelValue',editor.value?.getHTML()),
+    editorProps: {
+        attributes: {
+            class: 'min-h-[300px] prose prose-sm max-w-none py-1.5 px-3',
+        },
+    },
+    onUpdate:()=>emit('update:modelValue',editor.value?.storage.markdown.getMarkdown()),
 });
 
 watch(()=>props.modelValue,(value)=>{
-    if (value === editor.value?.getHTML()){
+    if (value === editor.value?.storage.markdown.getMarkdown()){
         return;
     }
      editor.value?.commands.setContent(value)
@@ -26,7 +33,7 @@ watch(()=>props.modelValue,(value)=>{
 </script>
 
 <template>
-    <EditorContent :editor="editor"/>
+    <EditorContent :editor="editor" class="border border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"/>
 </template>
 
 <style scoped>
