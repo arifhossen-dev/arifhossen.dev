5f586e9094a4da8b4f24790e34be24ea531a944e
arifhossen-dev <ahak.bsl@gmail.com>
2024-04-09 23:24:38 +0600

    Title and Body attribute setter created in Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
@@ -15,8 +17,21 @@ class Post extends Model
         'title',
         'slug',
         'body',
+        'html',
     ];
 
+    public function title(): Attribute
+    {
+        return Attribute::set(fn ($value) => Str::title($value));
+    }
+
+    public function body(): Attribute
+    {
+        return Attribute::set(fn($value) =>[
+            'body' => $value,
+            'html' => str($value)->markdown(),
+        ]);
+    }
 
     function author(): BelongsTo
     {
